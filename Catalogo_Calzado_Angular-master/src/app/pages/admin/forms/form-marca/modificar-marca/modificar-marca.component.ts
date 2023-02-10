8f5e99a9cd9601077130edd4abdcc1dd2745a0e5
Cristhian-18 <cordovacristhian32@gmail.com>
2023-02-10 18:26:28 -0500

    validar

--- Catalogo_Calzado_Angular-master/src/app/pages/admin/forms/form-marca/modificar-marca/modificar-marca.component.ts
+++ Catalogo_Calzado_Angular-master/src/app/pages/admin/forms/form-marca/modificar-marca/modificar-marca.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ConexMarcaService,Marca } from 'src/app/services/conexiones/conex-marca/conex-marca.service';
-
+import swal from 'sweetalert2';
 @Component({
   selector: 'app-modificar-marca',
   templateUrl: './modificar-marca.component.html',
@@ -44,7 +44,7 @@ export class ModificarMarcaComponent implements OnInit {
     this.subcription.unsubscribe();
     console.log('Observable cerrado')
   }
-  
+  /*
   modificar(id:number,nombre:string,descripcion:string){
     //Extrae text//
     this.marca.id_Marca = id;
@@ -59,5 +59,59 @@ export class ModificarMarcaComponent implements OnInit {
        },
        err=>console.log(err)
     ); 
-  }  
+  }
+  */
+   
+  modificar(id:number,nombre:string,descripcion:string){
+     //Extrae text//
+     this.marca.id_Marca = id;
+     this.id_entrada =id;
+     this.marca.nombre = nombre;
+     this.marca.descripcion = descripcion;
+    
+    swal.fire({
+      title: 'Seguro que quieres modificarlo?',
+      text: "Seguro que quieres hacer esto!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, modificarlo!'
+    }).then((result) => {
+      if (result.value) {         
+        try {   
+          if(this.marca.id_Marca !=0 && this.marca.nombre !='' && this.marca.descripcion!=''){
+            this.conexion.editmarca(this.id_entrada,this.marca).subscribe(
+              res=>{
+                console.log(res);       
+              },
+              err=>console.log(err)
+            );
+            
+            swal.fire({
+              icon: 'success',
+              title: 'Se modificó el registro de Marcas Exitosamente',
+              text: 'Continuar'
+            });
+          }else{
+            swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: 'Por Favor!! Ingrese todos los parámetros'
+            });
+          }
+        } catch (error) {
+          swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Ingrese todos los parametros Por favor'
+          });
+        } 
+      }
+    })    
+  }
+ 
+  
+
+
 }

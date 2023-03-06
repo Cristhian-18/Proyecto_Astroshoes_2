04acba8e6da63405396b350d3cc555e98394e7d8
jeinermenxd <mjair360@gmail.com>
2023-03-06 00:33:59 -0500

    links

--- Catalogo_Calzado_Angular-master/src/app/services/api.ts
+++ Catalogo_Calzado_Angular-master/src/app/services/api.ts
@@ -1,2 +1,3 @@
 /* Exportando la constante API_URL para usarla en otros archivos. */
-export const API_URL = 'http://34.27.188.160/';
+//export const API_URL = 'http://34.27.188.160/'; //APi de la red
+export const API_URL = 'http://localhost:3000/'; //API LOCAL
